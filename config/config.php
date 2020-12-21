ce956360df362f0c999a3525b8dcf28ad5512ce6
Elham-EN <ilhamrezaie@gmail.com>
2020-12-21 21:09:31 +1100

    Tiny changes on config

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ session_start();
 
 $timezone = date_default_timezone_set("Australia/Melbourne");
 
-$con = mysqli_connect("localhost", "root", "12345", "social", "3308");
+$con = mysqli_connect("localhost", "root", "12345", "social", "3306");
 
 //$con = mysqli_connect("localhost", "id13291066_root", "eN/*zm_gmK}~*mY5", "id13291066_social", "3306");
 
